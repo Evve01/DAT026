70a5acd2bc5409b8cb8df55161a294a13f4b972e
AmandaPandaJones <145320546+AmandaPandaJones@users.noreply.github.com>
2025-09-29 14:20:33 +0200

    more modular

--- Model.java
+++ Model.java
@@ -27,59 +27,76 @@ class Model {
 		balls = new Ball[2];
 		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2, 1);
 		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3, 1);
+
+        //initialize the start energy to keep track of energy differences in the system
         startEnergy =  0.5 * (Math.pow(balls[0].vx, 2) + Math.pow(balls[0].vy, 2)) * balls[0].mass +
                        0.5 * (Math.pow(balls[1].vx, 2) + Math.pow(balls[1].vy, 2)) * balls[1].mass;
 	}
 
 	void step(double deltaT) {
-		// TODO this method implements one step of simulation with a step deltaT
-        double distanceX = Math.pow((balls[0].x - balls[1].x), 2);
-        double distanceY = Math.pow((balls[0].y - balls[1].y), 2);
-
-        double ball0Energy = (Math.pow(balls[0].vx,2) + Math.pow(balls[0].vy,2)) * balls[0].mass * 1 / 2
-                + balls[0].mass * balls[0].y * acc_gravity;
-        double ball1Energy = (Math.pow(balls[1].vx,2) + Math.pow(balls[1].vy,2))* balls[1].mass * 1 / 2
-                + balls[1].mass * balls[1].y * acc_gravity;
-
-        double currentEnergy =  ball0Energy + ball1Energy;
-
-        if (currentEnergy != startEnergy) {
-            counter += 1;
-            if (counter >= 5) {
-                System.out.println(currentEnergy - startEnergy);
-            }
-        }
-
-        if (distanceX + distanceY <= Math.pow(balls[0].radius + balls[1].radius, 2)) {
+        showEnergyDiff();
+        if(ballsColliding()){
             ballCollision(balls[0], balls[1]);
         }
 
 		for (Ball b : balls) {
-			// detect collision with the border
-			if (b.x < b.radius) {
-				b.vx *= -1; // change direction of ball
-                b.x = b.radius;
-			}
-            if (b.x > areaWidth - b.radius) {
-                b.vx *= -1; // change direction of ball
-                b.x = areaWidth - b.radius;
-            }
+            collisoinWithBorderEvent(b);
 			if (b.y < b.radius) {
 				b.vy *= -1;
                 b.y = b.radius;
 			}
+
             if (b.y > areaHeight - b.radius) {
                 b.vy *= -1;
                 b.y = areaHeight - b.radius;
             }
             //b.vy -= acc_gravity * deltaT / 2;
             b.vy -= acc_gravity * deltaT;
-
-			// compute new position according to the speed of the ball
-			b.x += deltaT * b.vx;
-			b.y += deltaT * b.vy;
 		}
+        moveBalls(deltaT);
 	}
+    boolean ballsColliding(){
+        double distanceX = Math.pow((balls[0].x - balls[1].x), 2);
+        double distanceY = Math.pow((balls[0].y - balls[1].y), 2);
+
+        return distanceX + distanceY <= Math.pow(balls[0].radius + balls[1].radius, 2);
+    }
+
+    void showEnergyDiff(){
+        double currentEnergy = getCurrentEnergy();
+        if (currentEnergy != startEnergy) {
+            counter += 1;
+            if (counter >= 5) {
+                System.out.println(currentEnergy - startEnergy);
+            }
+        }
+    }
+    double getCurrentEnergy(){
+        double ball0Energy = (Math.pow(balls[0].vx,2) + Math.pow(balls[0].vy,2)) * balls[0].mass * 1 / 2
+                + balls[0].mass * balls[0].y * acc_gravity;
+        double ball1Energy = (Math.pow(balls[1].vx,2) + Math.pow(balls[1].vy,2))* balls[1].mass * 1 / 2
+                + balls[1].mass * balls[1].y * acc_gravity;
+
+        return ball0Energy + ball1Energy;
+    }
+    void collisoinWithBorderEvent(Ball b){
+        // detect collision with the border
+        if (b.x < b.radius) {
+            b.vx *= -1; // change direction of ball
+            b.x = b.radius;
+        }
+        if (b.x > areaWidth - b.radius) {
+            b.vx *= -1; // change direction of ball
+            b.x = areaWidth - b.radius;
+        }
+    }
+    void moveBalls(double deltaT){
+        for (Ball b : balls) {
+        // compute new position according to the speed of the ball
+        b.x += deltaT * b.vx;
+        b.y += deltaT * b.vy;
+        }
+    }
 
     void ballCollision(Ball ball1, Ball ball2) {
         double u1x = ball1.vx;
